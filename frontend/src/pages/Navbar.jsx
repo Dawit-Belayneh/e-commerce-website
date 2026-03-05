3936e4ffd4681598d1f51fc1875735529bb76c91
Dawit-Belayneh <dawitbelayneh9@gmail.com>
2026-03-05 10:46:12 +0300

    change .js to jsx, update navbar style

---
+++ frontend/src/pages/Navbar.jsx
@@ -0,0 +1,92 @@
+import "./Home.css";
+import { Link, useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+
+
+function Navbar(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem("access_token");
+
+        if (token){
+            setIsLoggedIn(true);
+
+            const savedUsername = localStorage.getItem("username");
+            setUsername(savedUsername || "Account");
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        localStorage.removeItem("username");
+        setIsLoggedIn(false);
+    };
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        if (searchQuery.trim()){
+            navigate(`/products?search=${searchQuery}`);
+            navigate(`/categories?search=${searchQuery}`);
+        }
+    }
+
+    return(
+        <nav className="navbar">
+            {/* LEFT: LOGO & NAV */}
+            <div className="nav-left">
+                <div className="nav-logo">
+                    <Link to="/">Ethio<span>Shop</span></Link>
+                </div>
+                <ul className="nav-links">
+                    <li><Link to="/products">Shop</Link></li>
+                    <li><Link to="/categories">Categories</Link></li>
+                </ul>
+            </div>
+
+            {/* CENTER: SEARCH BAR */}
+            <div className="nav-center">
+                <form className="search-container" onSubmit={handleSearch}>
+                    <input
+                        type="text"
+                        placeholder="Search products..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <button type="submit" className="search-btn">
+                        <span className="serch-icon">🔍</span>
+                    </button>
+                </form>
+            </div>
+
+            {/* RIGHT: AUTH & CART */}
+            <div className="nav-right">
+                <ul className="auth-links">
+                    {isLoggedIn ? (
+                        <>
+                            <li className="user-greet">
+                                <Link to={`/profile/${username}`}>Hi, {username}</Link>
+                            </li>
+                            <li><button className="logout-link" onClick={handleLogout}>Logout</button></li>
+                        </>
+                    ) : (
+                        <li><Link to="/login" className="login-link">Sign In</Link></li>
+                    )}
+                </ul>
+
+                <div className="nav-icon">
+                    <Link to="/cart" className="cart-wrapper">
+                        <span className="cart-icon">🛒</span>
+                        <span className="cart-count">0</span>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
